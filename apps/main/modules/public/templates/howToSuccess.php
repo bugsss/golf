f0582584533058929b97da46f39fb899abedc643
Bugs <bugs@cia.hu>
2013-10-29 00:35:20 +0100

    conflict in view file

--- apps/main/modules/public/templates/howToSuccess.php
+++ apps/main/modules/public/templates/howToSuccess.php
@@ -1,16 +1,16 @@
-    <div id="introCellWrapper">
-  		<script>
-  			$(document).ready(function() {
-  			//    $("html").niceScroll();  // The document page (html)
+	<script>
+  		$(document).ready(function() {
+  		//    $("html").niceScroll();  // The document page (html)
     
-    		$("#boxScroll").niceScroll({touchbehavior:false,cursorcolor:"#96B613",cursoropacitymax:0.7,cursorwidth:7,cursorborder:"1px solid #000",
-			cursorborderradius:"3px",background:"#6B820C",autohidemode:"false"}).cursor.css({"background-image":"url(img/mac6scroll.png)"}); // MAC like scrollbar
+    	$("#boxScroll").niceScroll({touchbehavior:false,cursorcolor:"#96B613",cursoropacitymax:0.7,cursorwidth:7,cursorborder:"1px solid #000",
+		cursorborderradius:"3px",background:"#6B820C",autohidemode:"false"}).cursor.css({"background-image":"url(img/mac6scroll.png)"}); // MAC like scrollbar
 
-    		$("#boxscroll2").niceScroll({touchbehavior:false,cursorcolor:"#00F",cursoropacitymax:0.7,cursorwidth:6,background:"#ccc",autohidemode:false});
+    	$("#boxscroll2").niceScroll({touchbehavior:false,cursorcolor:"#00F",cursoropacitymax:0.7,cursorwidth:6,background:"#ccc",autohidemode:false});
 	
-  			});
-		</script>
-        
+  		});
+	</script>
+
+    <div id="introCellWrapper">
         <div id="boxScroll">
             <table align="center" cellspacing"7">
                 <tr>
