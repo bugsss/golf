ba73434013561e5671e96a337356b73e4a668198
Bugs <bugs@cia.hu>
2013-11-12 06:11:41 +0100

    jissan design modifications

--- apps/main/modules/public/templates/productsSuccess.php
+++ apps/main/modules/public/templates/productsSuccess.php
@@ -1,3 +1,195 @@
-    <div id="introCellWrapper">
-        <img src="/images/comingSoon.jpg" border="0" />
-    </div>
+<script>
+    $(document).ready(function() {
+        //    $("html").niceScroll();  // The document page (html)
+    
+        $("#boxScroll").niceScroll({touchbehavior:false,cursorcolor:"#96B613",cursoropacitymax:0.7,cursorwidth:7,cursorborder:"1px solid #000",
+            cursorborderradius:"3px",background:"#6B820C",autohidemode:"false"}).cursor.css({"background-image":"url(img/mac6scroll.png)"}); // MAC like scrollbar
+
+        $("#boxscroll2").niceScroll({touchbehavior:false,cursorcolor:"#00F",cursoropacitymax:0.7,cursorwidth:6,background:"#ccc",autohidemode:false});
+	
+    });
+</script>
+
+<div id="introCellWrapper">
+    <div id="boxScroll">
+    	<h1 class="fontface1">Product and Services</h1>
+        
+        <p class="style1">
+        	Golf Games Keeper has developed one of the world's premier smartphone application to integrate golfers, golf leagues and golf games, while improving the golf experience and attracting new golfers to the sport.
+        </p>
+        
+        <br />
+        
+        <h2 class="fontface2">Golf GamesKeeper</h2>
+        
+        <img src="images/pas_1.jpg" border="0" />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <h3 class="fontface3">GPS Enabled Course Finder</h3>
+        
+        <img src="images/pas_2.png" border="0" />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <h3 class="fontface3">Select Tees, and Starting Hole</h3>
+        
+        <img src="images/pas_3.png" border="0" />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <h3 class="fontface3">Add Players</h3>
+        
+        <img src="images/pas_4.png" border="0" class="TL" />
+     	
+   		<p class="style1">Recent Players:<br />Allows you to pick players that you have recently played a round of golf with.</p>
+            
+      	<p class="style1">Nearby Players:<br />When a player Checks in (see home page) on to a course, he is now discoverable by a game organizer.</p>
+            
+       	<p class="style1">From Contacts:<br />Get access to the contact list on your phone.</p>
+            
+       	<p class="style1">GamesKeeper ID:<br />Using the Golf GamesKeeper ID of the player.</p>
+            
+        <p class="style1">New Player:<br />Create a new player.</p>
+        
+        <div class="clearFix"></div>
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <h3 class="fontface3">Course Check-in</h3>
+        
+        <img src="images/pas_5.png" border="0" class="TL" />
+        
+        <p class="style1">Players can check in by selecting a course and wait.</p>
+        
+        <p class="style1">They now become discoverable to a game organizer on that course.</p>
+        
+        <p class="style1">Once they have been picked up for a game, they will be forwarded to the game summary screen.</p>
+        
+        <div class="clearFix">
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <h3 class="fontface3">Add Bets</h3>
+        
+        <img src="images/pas_6.png" />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <p class="style1">
+        	After adding a player, you have the option to create a bet by clicking on the players name.  Once all the bets are entered, the player can start the Game.
+        </p>
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <h3 class="fontface3">Scoring and Presses</h3>
+        
+        <img src="images/pas_7.png" border="0" />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <h2 class="fontface2">Games Available</h2>
+        
+        <h3 class="fontface3">Available Now</h3>
+        
+        <p style="color:#000; padding:0; margin:0;">Team Size of 1 Player per Team</p>
+        
+        <ul class="style21">
+        	<li>Stroke Play Nassau</li>
+            <li>Hole Play (2 players)</li>
+            <li>Match Play Nassau</li>
+            <li>Skins (best score on any given hole)</li>
+            <li>$ per hole</li>
+        </ul>
+        
+        <h3 class="fontface3">Future Development</h3>
+        
+        <p style="color:#000; padding:0; margin:0;">Team Size of 2+</p>
+        
+        <ul class="style21">
+        	<li>Scotch</li>
+            <li>Wolf</li>
+            <li>Bingo Bango Bongo</li>
+            <li>Blind Nine</li>
+            <li>Las Vegas</li>
+            <li>Scramble (up to 6 players)</li>
+            <li>Customizable Point Games</li>
+            <li>Tournament Play (6+)</li>
+            <li>Etc</li>
+        </ul>
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+        
+        <br />
+    </div><!--END boxScroll-->
+</div><!--END introCellWrapper-->
