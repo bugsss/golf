8e0835d7e063baca4f11cf7edcaf93d982a87e3f
Bugs <bugs@cia.hu>
2013-11-21 12:53:50 +0100

    press page design adjustments

--- apps/main/modules/public/templates/pressSuccess.php
+++ apps/main/modules/public/templates/pressSuccess.php
@@ -7,11 +7,11 @@
             <table styel="width: 100%">
                 <?php foreach( $articles as $article ): ?>
                     <tr>
-                        <td style="width: 80%"><?php echo $article->getTitle(); ?></td>
-                        <td style="width: 20%; float: right"><?php echo date( "Y-m-d", strtotime( $article->getPublishDate() ) ); ?></td>
+                        <td style="width: 80%"><h1 class="article_title"><?php echo $article->getTitle(); ?></h1></td>
+                        <td style="width: 20%; float: right"><span class="article_date"><?php echo date( "Y-m-d", strtotime( $article->getPublishDate() ) ); ?></span></td>
                     </tr>
                     <tr>
-                        <td colspan="2" style="width: 100%"><?php echo html_entity_decode( $article->getContent() ); ?></td>
+                        <td colspan="2" style="width: 100%"><span class="article_content"><?php echo html_entity_decode( $article->getContent() ); ?></span></td>
                     </tr>
                     <tr><td colspan="2" style="height: 15px;"> </td></tr>
                 <?php endforeach; ?>
